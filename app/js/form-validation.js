537036e932e8ec0ebe3d90cf4b9ac9688e304806
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-10-26 00:07:04 +0200

    fxing form validation bug

--- app/js/form-validation.js
+++ app/js/form-validation.js
@@ -1,30 +1,25 @@
-// https://www.freecodecamp.org/news/learn-javascript-form-validation-by-making-a-form/
-
-console.log("hello");
-
 let form = document.getElementById("form");
 let email = document.getElementById("email");
 let errorMessage = document.getElementById("error-email");
 
-let engine = (id, message) => {
-  if(id.value.trim() === "") {
-    errorMessage.innerHTML = message;
-    id.style.border = "1px solid red";
-  } else {
-    errorMessage.innerHTML = "";
-  }
-}
-
+// validating email value
 form.addEventListener("submit", (e) => {
-  if(email.value.trim() === "" || email.value == null)  {
-    e.preventDefault();
-    errorMessage.innerHTML = "Email is required";
-    email.classList.add("invalid");
-  }
   const emailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(!email.value.match(emailformat)) {
+  email.classList.remove("invalid");
+
+  if(email.value === "" || email.value == null)  {
+    e.preventDefault();
+    errorMessage.innerHTML = "Whoops! It looks like you forgot to add your email";
+    email.classList.add("invalid");
+    return false;
+
+  } else if(!email.value.match(emailformat)) {
     e.preventDefault();
     errorMessage.innerHTML = "Please enter a valid email address";
     email.classList.add("invalid");
+    return false;
+
+  } else {
+    return true;
   }
 })
